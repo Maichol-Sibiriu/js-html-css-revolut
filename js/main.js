42e59ea9fe5d2419f4b43c7101cd8552d3cb8dc1
Maichol <msibiriu@gmail.com>
2020-10-28 19:19:49 +0100

    ok dropdown di entrambi i menu,da rifinire css

--- js/main.js
+++ js/main.js
@@ -6,14 +6,19 @@ $(document).ready( function() {
   var dropdownF = $(".dropdown-firm > a");
   // riferimenti js dropdown
   var products = $(".dropdown-products .menu-products");
-  var firm = $(".dropdown-firm > menu-firm");
+  var firm = $(".dropdown-firm .menu-firm");
 
   // evento click products
   dropdownP.click(function() {
     $(this).next(".menu-products").toggle();
+    // blocco menu
+    firm.not(products).hide();
   });
   // evento click firm
   dropdownF.click(function() {
     $(this).next(".menu-firm").toggle();
+    // blocco menu
+     products.not(firm).hide();
   });
+
 });// fine documento
